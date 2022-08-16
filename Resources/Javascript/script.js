54350c6c03ef6f56b1d7ca02d124ac987eda2877
Molly Salmonsen <mollysalmonsen@Mollys-MacBook-Pro-2.local>
2022-08-15 21:32:25 -0400

    Space API Printing Cards & they Have IDs

--- Resources/Javascript/script.js
+++ Resources/Javascript/script.js
@@ -6,6 +6,9 @@ var resultsSection = document.querySelector('#resultsSection');
 
 var apiKey = '8c1219fa65fe7f4d84bf59a3a16e277d';
 
+//------------------------------------------- FUNCTIONS -------------------------------------------
+//When a user types in the Country code (i.e. USA) a bunch of cards populate with space launches in that country
+
 let formSubmitHandler = function(e) {
   e.preventDefault();
   let countryName = searchInput.value.trim();
@@ -34,18 +37,23 @@ function getSpaceAPI(countryName) {
       return response.json();
     })
     .then(function (data) {
-      for (var i = 0; i < data.length; i++) {
-        var listItem = document.createElement('li');
+      console.log(data);
+      //For Each Result Print into a Card
+      for (var i = 0; i < data.results.length; i++) {
+        //create element - a HTML element only
+        var listItem = document.createElement('div');
+        listItem.className = "card py-3";
+        listItem.setAttribute("id","card"+i);
         listItem.textContent = data.results[i].name;
         resultsSection.appendChild(listItem);
-        console.log(data);
+        console.log(data.results[i].name);
       }
     });
 }
 
 getSpaceAPI();
 
-//When a user types in the Country code (i.e. USA) a bunch of cards populate with space launches in that country
+
 //when a user clicks on a launch location, the weather populates 
 
 
